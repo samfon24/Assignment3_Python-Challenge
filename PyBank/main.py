2b3192f622797f625efd48051a20797770cf4d25
samfon24 <samfons24@yahoo.com>
2020-08-16 18:03:42 -0500

    Created additional lists to help sort for max and min values

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,6 +4,7 @@ import csv
 file_path = os.path.join('Resources','budget_data.csv')
 
 contents = []
+dates = []
 profit_loss_amt = []
 profit_loss_diff_amt = []
 total_amt = 0
@@ -22,21 +23,32 @@ with open(file_path, 'r') as csvfile:
         total_amt += float(row[1])
         # Creates a list of just profit/loss
         profit_loss_amt.append(float(row[1]))
+        # Creates a list of all dates
+        dates.append(row[0])
 
 # The total number of months included in the dataset AKA last row not counting the header since we did next*
 months = len(contents)
-print(f'(Total Months: {months})'
+print(f'Total Months: {months}')
 
 # The net total amount of "Profit/Losses" over the entire period
-print(round(total_amt))
+print(f'Net Total for Data Set Period: {round(total_amt)}')
 
 # The avg of the changes in Profit/Losses over the entire period
 for i in range(months-1):
     # Obtains total for changes between row 
     avg_chng += ((profit_loss_amt[i+1]) - (profit_loss_amt[i]))
+
     # Creates a list of every delta at each row
-    profit_loss_diff_amt.append((profit_loss_amt[i+1]) - (profit_loss_amt[i]))
+    date = dates[i+1]
+    delta_chng = (profit_loss_amt[i+1]) - (profit_loss_amt[i])
+    profit_loss_diff_amt.append((date, delta_chng))
+
+print()
+profit_loss_diff_amt.insert(0, 0)
+print((profit_loss_diff_amt))
+# print(len(contents))
 # Prints the average in changes for data set
-print(round(avg_chng/float(months-1),2))
-# Prints the Max of the profit/loss list 
-print(max(profit_loss_diff_amt))
+# print(round(avg_chng/float(months-1),2))
+
+# Prints the Max of the profit/loss list
+# print(max(profit_loss_diff_amt))
