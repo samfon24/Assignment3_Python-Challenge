5c2f8315ec6d4a22367c7c220fd201f6d7905974
samfon24 <samfons24@yahoo.com>
2020-08-18 00:03:58 -0500

    Finalized percentages, and winner with creating a dictionary and referencing the index with an enumerate function

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,7 +4,7 @@ import csv
 file_path = os.path.join('Resources','election_data.csv')
 
 voter_id = []
-
+candidate_votes = []
 
 with open(file_path, 'r') as csvfile:
 
@@ -14,10 +14,39 @@ with open(file_path, 'r') as csvfile:
 
     for row in csvreader:
         voter_id.append(row)
+        candidate_votes.append(row[2])
 
 # Headers
 print('Election Results')
-print('-'*20 + '\n')
+print('-'*20)
 
 # Print the total voter count
-print(f'(Total Votes: {len(voter_id)}')
+print(f'Total Votes: {len(voter_id)}')
+
+khan_count = candidate_votes.count('Khan')
+khan_percent = format((float(khan_count)/float(len(voter_id)))*100, ".3f")
+print(f'Khan: {khan_percent}% ({khan_count})')
+
+correy_count = candidate_votes.count('Correy')
+correy_percent = format((float(correy_count)/float(len(voter_id)))*100, ".3f")
+print(f'Correy: {correy_percent}% ({correy_count})')
+
+li_count = candidate_votes.count('Li')
+li_percent = format((float(li_count)/float(len(voter_id)))*100, ".3f")
+print(f'Li: {li_percent}% ({li_count})')
+
+tooley_count = candidate_votes.count("O'Tooley")
+tooley_percent = format((float(tooley_count)/float(len(voter_id)))*100, ".3f")
+print(f"O'Tooley: {tooley_percent}% ({tooley_count})")
+
+candidate_count = {
+    'candidates': ['Khan', 'Correy', 'Li', "O'Tooley"],
+    'votes': [khan_count, correy_count, li_count, tooley_count]
+    }
+
+print('-'*20)
+winner_votes = max(candidate_count['votes'])
+for index, winner in enumerate(candidate_count['votes']):
+    if candidate_count['votes'][index] == winner_votes:
+        print(f"Winner: {candidate_count['candidates'][index]}")
+print('-'*20)
